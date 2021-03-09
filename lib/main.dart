77490f3870b80f483fd43c77fd66abe325e9421b
Anders Børjesson <anbo@zealand.dk>
2021-03-09 13:48:52 +0100

    collect words

---
+++ lib/main.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Collect Words',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Collect Words'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> words = [];
+  String wordsStr = "";
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  void _addWord() {
+    setState(() {
+      words.add(myController.text);
+      //wordsStr = _words.toString();
+    });
+  }
+
+  void _clearWords() {
+    setState(() {
+      words = [];
+      wordsStr = "";
+      print(words.toString());
+    });
+  }
+
+  void _showWords() {
+    setState(() {
+      wordsStr = words.toString();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('Collect words',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            TextField(
+              controller: myController,
+              decoration: InputDecoration(hintText: "Type a word"),
+              keyboardType: TextInputType.text,
+            ),
+            Row(children: <Widget>[
+              ElevatedButton(
+                  onPressed: () {
+                    _addWord();
+                  },
+                  child: Text('Save')),
+              Spacer(),
+              ElevatedButton(
+                  onPressed: () {
+                    _clearWords();
+                  },
+                  child: Text('Clear')),
+              Spacer(),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      wordsStr = words.toString();
+                    });
+                  },
+                  child: Text('Show')),
+              Spacer(),
+              IconButton(icon: Icon(Icons.save)),
+              IconButton(
+                  icon: Icon(Icons.delete),
+                  tooltip: 'Clear',
+                  onPressed: () {
+                    _clearWords();
+                  }),
+              IconButton(icon: Icon(Icons.list))
+            ]),
+            Text('Words: ' + wordsStr)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Doing nothing',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
